e4a2d2f7e613cd77aa15d83761b31220a1844dc6
samiadewan <samia.dewan@brainstation-23.com>
2023-04-15 12:14:57 +0600

    Add project files.

---
+++ BornomalaStore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BornomalaStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
